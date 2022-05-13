697b4be50347044b937381b18b7e13d8c07d7be6
Exodia <bbextasy@mail.ru>
2022-05-13 04:32:14 +0300

    feat(): created category component

---
+++ src/components/TheCategory.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="category">
+    <h5 class="title h5">{{ category_data.title }}</h5>
+    <div class="imgWrapper">
+      <img
+        :src="require('../assets/images/' + category_data.img)"
+        alt="category-img"
+        class="img"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TheCategory",
+    props: {
+      category_data: {
+        type: Object,
+        default: () => ({}),
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  .category {
+    cursor: pointer;
+    position: relative;
+    height: 155px;
+    min-width: 200px;
+    max-width: 500px;
+    background-color: var(--color-gray);
+    border-radius: 5px;
+    overflow: hidden;
+  }
+  .title {
+    padding: 15px;
+    height: 42px;
+  }
+  .imgWrapper {
+    position: absolute;
+    height: 115px;
+    width: 100%;
+    bottom: 0;
+  }
+  .img {
+    position: absolute;
+    max-width: 100%;
+    max-height: 100%;
+    right: 0;
+    bottom: 0;
+  }
+</style>
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="categories">
+    <TheCategory
+      v-for="category in categoryData"
+      :key="category.id"
+      :category_data="category"
+    />
+  </div>
+</template>
+
+<script>
+  import TheCategory from "@/components/TheCategory.vue";
+  export default {
+    components: {
+      TheCategory,
+    },
+    data: () => ({
+      categoryData: [
+        {
+          title: "Компьютерная Техника",
+          id: "1",
+          img: "notebook.png",
+        },
+        {
+          title: "Офисная Техника",
+          id: "2",
+          img: "printer.png",
+        },
+        {
+          title: "Компьютерная Техника",
+          id: "3",
+          img: "notebook.png",
+        },
+        {
+          title: "Офисная Техника",
+          id: "4",
+          img: "printer.png",
+        },
+        {
+          title: "Телевизоры, аудио, видео",
+          id: "5",
+          img: "TV.png",
+        },
+        {
+          title: "Климатическая техника",
+          id: "6",
+          img: "conditioner.png",
+        },
+        {
+          title: "Бытовая Техника",
+          id: "7",
+          img: "freeze.png",
+        },
+        {
+          title: "Развлечения и игры",
+          id: "8",
+          img: "PS5.png",
+        },
+      ],
+    }),
+  };
+</script>
+
+<style scoped>
+  .categories {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 10px;
+    max-width: 1200px;
+  }
+</style>
